07eff411bc8e0cf5fe1aa5cb1bf960f109fdd884
hanilizalo23 <nelida_phm@hotmail.com>
2022-04-17 16:59:00 -0500

    Some functions added

--- SPI.c
+++ SPI.c
@@ -9,5 +9,19 @@
 
 void SPI_Init(void)
 {
+	dspi_master_config_t masterConfig;
+	uint32_t srcClock_Hz;
 
+	CLOCK_SetSimSafeDivs();
+
+	CLOCK_EnableClock(kCLOCK_PortD);
+	PORT_SetPinMux(PORTD,PIN0_IDX,kPORT_MuxAlt2);
+	PORT_SetPinMux(PORTD,PIN1_IDX,kPORT_MuxAlt2);
+	PORT_SetPinMux(PORTD,PIN2_IDX,kPORT_MuxAlt2);
+	PORT_SetPinMux(PORTD,PIN3_IDX,kPORT_MuxAlt2);
+
+	srcClock_Hz = DSPI0_CLK_SRC;
+	DSPI_MasterInit(SPI0, &masterConfig, srcClock_Hz);
+
+	return;
 }
--- SPI.h
+++ SPI.h
@@ -15,6 +15,20 @@
 #include "fsl_clock.h"
 #include "clock_config.h"
 
+#define PIN0_IDX  0u
+#define PIN1_IDX  1u
+#define PIN2_IDX  2u
+#define PIN3_IDX  3u
+#define PIN16_IDX 16u
+#define PIN17_IDX 17u
+
+#define BITS_FRAME 8
+
+//1 MHz Transmission
+#define TRANSFER_DELAY	  1000000000U
+#define TRANSFER_SIZE     64U
+#define TRANSFER_BAUDRATE 1000000U
+
 void SPI_Init(void);
 
 #endif /* SPI_H_ */
