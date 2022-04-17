43f49a1a71ac81968f6933d13cfcda91f249cc1a
hanilizalo23 <nelida_phm@hotmail.com>
2022-04-17 17:06:13 -0500

    Given codes for LCD

---
+++ LCD_nokia_images.h
@@ -0,0 +1,13 @@
+/**
+ * Created on: 17/04/2022
+ * Author: Nelida Hernández
+ * @file    LCD_nokia_images.h
+ * @brief   Define array for show ITESO LOGO in a bitmap format.
+ */
+
+#ifndef LCD_NOKIA_IMAGES_H_
+#define LCD_NOKIA_IMAGES_H_
+
+#include "stdint.h"
+
+#endif /* LCD_NOKIA_IMAGES_H_ */
